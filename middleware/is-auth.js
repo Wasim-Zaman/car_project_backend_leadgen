6bf0a252778f3f2f6705720c8052739a7620d4c4
Wasim <wasimxaman13@gmail.com>
2024-07-23 20:26:28 +0500

    chore: Add .env to .gitignore and update package.json

---
+++ middleware/is-auth.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const CustomError = require("../exceptions/customError");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorization");
+
+  if (!authHeader) {
+    const error = new CustomError("Not authenticated.");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  const token = authHeader.split(" ")[1];
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    err.statusCode = 500;
+    err.message = null;
+    throw err;
+  }
+
+  if (!decodedToken) {
+    const error = new CustomError("Not authenticated.");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  req.userId = decodedToken.userId;
+  req.email = decodedToken.email;
+
+  next();
+};
