80bd1d9d0f1527bbdb04afe584cca75c2704ecba
Wasim <wasimxaman13@gmail.com>
2024-07-25 11:20:53 +0500

    refactor: Update route paths for city CRUD operations

--- routes/city.js
+++ routes/city.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 router.get("/v1/cities", cityController.getCities);
 
-router.post("/v1/cities", cityController.postCity);
+router.post("/v1/city", cityController.postCity);
 
-router.delete("/v1/cities/:id", cityController.deleteCity);
+router.delete("/v1/city/:id", cityController.deleteCity);
 
-router.patch("/v1/cities/:id", cityController.patchCity);
+router.patch("/v1/city/:id", cityController.patchCity);
 
 module.exports = router;
