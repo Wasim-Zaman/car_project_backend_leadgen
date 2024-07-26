fba030a850de464eda87542a587276a34cb59f08
Wasim <wasimxaman13@gmail.com>
2024-07-26 13:21:11 +0500

    feat: Add gallery routes for managing images

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const bannerRoutes = require("./routes/banner");
 const cityRoutes = require("./routes/city");
 const brandRoutes = require("./routes/brand");
 const carRoutes = require("./routes/car");
+const galleryRoutes = require("./routes/gallery");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -29,6 +30,7 @@ app.use("/api/banner", bannerRoutes);
 app.use("/api/city", cityRoutes);
 app.use("/api/brand", brandRoutes);
 app.use("/api/car", carRoutes);
+app.use("/api/gallery", galleryRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use((req, res, next) => {
