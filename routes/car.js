32769f52f6f5cc80494eeead27c4c04c7000244c
Wasim Zaman <wasimxaman13@gmail.com>
2024-07-30 08:43:26 -0700

    refactor: Update car and car_type routes to include validation rules and middleware

--- routes/car.js
+++ routes/car.js
@@ -2,6 +2,7 @@ const express = require("express");
 const controller = require("../controllers/car");
 const { uploadSingle } = require("../config/multerConfig");
 const isAdmin = require("../middleware/is-admin-auth");
+const { carValidationRules, validate } = require("../validators/car");
 
 const router = express.Router();
 
@@ -9,12 +10,26 @@ const router = express.Router();
 router.get("/v1/cars", isAdmin, controller.getCars);
 
 // Route to create a new car
-router.post("/v1/car", isAdmin, uploadSingle, controller.postCar);
+router.post(
+  "/v1/car",
+  isAdmin,
+  carValidationRules.postCar,
+  validate,
+  uploadSingle,
+  controller.postCar
+);
 
 // Route to delete a car by ID
 router.delete("/v1/car/:id", isAdmin, controller.deleteCar);
 
 // Route to update a car by ID
-router.patch("/v1/car/:id", isAdmin, uploadSingle, controller.patchCar);
+router.patch(
+  "/v1/car/:id",
+  isAdmin,
+  carValidationRules.patchCar,
+  validate,
+  uploadSingle,
+  controller.patchCar
+);
 
 module.exports = router;
--- routes/car_type.js
+++ routes/car_type.js
@@ -3,28 +3,18 @@ const express = require("express");
 const carController = require("../controllers/car_type");
 const { uploadSingle } = require("../config/multerConfig");
 const isAdmin = require("../middleware/is-admin-auth");
-const { carValidationRules, validate } = require("../validators/car");
 
 const router = express.Router();
 
 router.get("/v1/carTypes", isAdmin, carController.getCarTypes);
 
-router.post(
-  "/v1/carType",
-  isAdmin,
-  carValidationRules.postCar,
-  validate,
-  uploadSingle,
-  carController.postCarType
-);
+router.post("/v1/carType", isAdmin, uploadSingle, carController.postCarType);
 
 router.delete("/v1/carType/:id", isAdmin, carController.deleteCarType);
 
 router.patch(
   "/v1/carType/:id",
   isAdmin,
-  carValidationRules.patchCar,
-  validate,
   uploadSingle,
   carController.patchCarType
 );
