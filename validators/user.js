b871d961e80f36767cb68b22109a706af833383f
Wasim Zaman <wasimxaman13@gmail.com>
2024-08-05 08:36:31 -0700

    refactor: Remove unused latitude and longitude validation in user registration

--- validators/user.js
+++ validators/user.js
@@ -26,8 +26,8 @@ const registerUserValidator = [
         return Promise.reject("Mobile number already in use");
       }
     }),
-  body("lat").notEmpty().isFloat().withMessage("Lat is required"),
-  body("long").notEmpty().isFloat().withMessage("Long is required"),
+  //   body("lat").notEmpty().isFloat().withMessage("Lat is required"),
+  //   body("long").notEmpty().isFloat().withMessage("Long is required"),
   body("password")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters long"),
