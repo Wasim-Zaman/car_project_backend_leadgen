8f2969ada2c85a4621138c74ce60c7b9b00bf1db
Wasim Zaman <wasimxaman13@gmail.com>
2024-10-03 17:29:21 +0500

    statically served uploads folder

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
