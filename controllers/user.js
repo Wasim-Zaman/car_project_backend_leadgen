30e836a2f93784cdedfacb8408e2c9ea22ece001
Wasim Zaman <wasimxaman13@gmail.com>
2024-10-03 18:31:26 +0500

    refactor: update user registration schema for mobile number validation

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,13 @@ const fileHelper = require('../utils/file');
 const userRegistrationSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
-  mobile: Joi.string(),
+  mobile: Joi.string()
+    .pattern(/^\+[1-9]{1}[0-9]{3,14}$/)
+    .required()
+    .messages({
+      'string.pattern.base':
+        'Mobile number must be in international format starting with + followed by the country code and digits.',
+    }),
   password: Joi.string().min(8).required(),
   referralCode: Joi.string().optional(),
   lat: Joi.number().optional(),
